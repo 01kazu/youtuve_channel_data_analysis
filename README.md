e605b4b17a3af8a797006986e709c179f020b758
01kazu <davidjoshua026@gmail.com>
2024-09-16 16:59:27 +0100

    Added documentation to functions and README.md

--- README.md
+++ README.md
@@ -6,33 +6,43 @@
 - [Data source](#data-source)
 - [Stages](#stages)
 - [Design](#design)
+  - [Mockup](#mockup)
+  - [Tools](#tools)
+- [Development](#development)
+  - [Pseudocode](#pseudocode)
+  - [Data Collection](#data-collection)
 
 
 # Objectives
+
 - What is the key pain point?
 
 The client wants to:
+
 1. Identify what videos are performing the best on their youtube channel
 2. Identify what kind of content famous Nigerian food content creators have made this year.
 
 - What is the ideal solution? 
+
 1. Build a dashboard to provide insights in their channel's performance on different metrics such as:
     - view count
     - like count
     - comment count
-2. Provide recommendations on content she can upload on their channel.
-
+2. Provide recommendations on content she can upload on their channel
 
 Metrics
+
 1. views_per_subscribers: Dividing the View count by the subscribers count of the channel
 2. likes_per_subscribers: Dividing the Like count by the subscribers count of the channel
+3. likes_per_view: Dividing the Like count by the View count
 
-# Data source 
+# Data source
 
 - What data is needed to achieve our objective?
 
-We need data on the top Nigerian Food Content Creators in 2024 that includes their:
-- channel name
+We need data on the top Nigerian Food Content Creators in 2024 that includes the:
+
+- channel's name
 - total subscribers
 - video title
 - video duration
@@ -55,6 +65,7 @@ The data is scraped from youtube using the Selenium, BrightData and Youtube API.
 # Design
 
 ## Dashboard components required
+
 - What should the dashboard contain based on the requirements provided?
 
 To understand what it should contain, we need to figure out what questions we need the dashboard to answer:
@@ -69,7 +80,8 @@ To understand what it should contain, we need to figure out what questions we ne
 ## Dashboad Mockup
 ![Dashboard_Mockup](images/dashboard_mockup.png)
 
-This dashboard consists of various visualizations which include:
+This dashboard consists of various visualizations which includes:
+
 1. Line chart
 2. Tables
 3. Scatterplots
@@ -77,12 +89,47 @@ This dashboard consists of various visualizations which include:
 5. Cards
 
 ## Tools
+
 | Tool | Purpose |
 | --- | --- |
 | Python | Extracting, cleaning, testing, and analyzing the data |
-| Mokkup AI | Designing the wireframe/mockup of the dashboard | 
+| Mokkup AI | Designing the wireframe/mockup of the dashboard |
 | Tableau | Visualizing the data via interactive dashboards |
 | GitHub | Hosting the project documentation and version control |
+
+# Development
+
+## Psuedocode
+
+The steps used in providing solutions involves:
+
+1. Get the data
+2. Clean the data
+3. Analyze the data
+4. Visualize the data in Tableau
+5. Generate the findings based on the insights
+6. Write the documentation + commentary
+
+## Data collection
+
+The data needed for the analysis will be extracted from youtube using the Selenium. The list of youtubers to be focused on for this project was extracted from this [website](https://videos.feedspot.com/nigeria_food_youtube_channels/). The data collected were stored in two different files. The first file named ***youtuber_channel_details.csv*** contains:
+
+1. Youtuber's channel name
+2. Subscriber count
+3. Total video count
+
+The next file or rather collection of files are named after each individual youtuber's channel name and contains:
+
+1. Video Title
+2. Date video was released (UTC)
+3. View count
+4. Like count
+5. Comment count
+6. Date data was collected (UTC)
+7. Video duration in hours
+8. Video duration in minutes
+9. Video duration in seconds
+
 
 
 
--- data_cleaning.py
+++ data_cleaning.py
@@ -6,6 +6,18 @@ from typing import Dict, List
 
 
 def get_digits(string: str) -> str:
+    """
+    Extracts only the digits from a string
+
+    Parameters
+    ----------
+    string : str
+
+    Returns
+    -------
+    digits : str
+        string containing only digits
+    """
     char_list = [char for char in string if char.isdigit()]
     digits = ''.join(char_list)
     return digits
@@ -35,7 +47,17 @@ def get_video_duration(video_duration_str: str) -> Dict[str, str]:
 
 def data_cleaning_video(filepath: str) -> pd.DataFrame:
     """
+    Contains the cleaning process for the f'{youtuber channel name}'.csv file. 
 
+    Parameters
+    ----------
+    filepath : str
+        location of the csv file to be processed.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        The cleaned csv file in a pd.DataFrame format
     """
     df = pd.read_csv(filepath, parse_dates=['date_released_utc', 'date_collected_utc'])
     # Extract video duration from string
@@ -55,20 +77,44 @@ def data_cleaning_video(filepath: str) -> pd.DataFrame:
 def get_num(string: str) -> float:
     # 1.02M subscribers, 1.3k videos
     """
+    Convert a string containing (M for million, k for thousadnd) to it's appropriate digit.
+
+    Parameters
+    ----------
+    string : str
+        String in a particular format. e.g 1.2M, 3.4k.
+
+    Returns
+    -------
+    num : float
+        The integer value of the string. e.g 1200000, 3400.
     """
     first_substring = string.split()[0]
-    if first_substring.isdigit():
-        return float(first_substring)
     num, abbreviation = first_substring[:-1], first_substring[-1] 
+    num = float(num)
+    if first_substring.isdigit():
+        return num
     multiply_dict = {"M": 1000000, "K": 1000}
     abbreviation_int = multiply_dict.get(abbreviation.upper()) # To match the key of the multiply dictionary
     if abbreviation:
-        num_int = float(num) * abbreviation_int # type: ignore
-        return num_int
-    return 0.0 # something went wrong
+        num = num * abbreviation_int # type: ignore
+        return num
 
 
 def data_cleaning_channel_details(filepath: str) -> pd.DataFrame:
+    """
+    Contains the cleaning process for 'youtuber_channel_details.csv'
+
+    Parameters
+    ----------
+    filepath : str
+        Location of the 'youtuber_channel_details.csv' 
+
+    Returns
+    -------
+    df : pd.DataFrame
+        The cleaned DataFrame of the 'youtuber_channel_details.csv'
+    """
     df = pd.read_csv(filepath)
     # New columns
     df['channel_name'] = df['channel_name'].str.strip('@')
@@ -80,21 +126,37 @@ def data_cleaning_channel_details(filepath: str) -> pd.DataFrame:
     return df
 
 
-def data_compilation(filepath: str, 
-                     columns: List[str] = [''], 
+def data_compilation(dirpath: str, 
+                     columns: List[str], 
                      files_to_exclude: List[str] = ['']) -> pd.DataFrame:
+    """
+    Combines multiple csv files with the same columns into one dataframe.
+
+    Parameters
+    ----------
+    dirpath : str
+        Path to the directory/folder where the various csv files are located 
+    columns : List[str]
+        List of the columns of the file
+    files_to_exclude : List[str], optional
+        List of files from the directory/folder that should be excluded
+
+    Returns
+    -------
+    result_df : pd.DataFrame
+        DataFrame containing the combined data from al the csv files.
+    """
     result_df = pd.DataFrame(columns=columns)
-    for file in listdir(filepath):
-        file_dir = join(filepath, file) 
-        if isfile(file_dir):
+    for file in listdir(dirpath):
+        filepath = join(dirpath, file) 
+        if isfile(filepath):
             if file not in files_to_exclude:
-                df = pd.read_csv(file_dir, parse_dates=['date_released_utc',
+                df = pd.read_csv(filepath, parse_dates=['date_released_utc',
                                                         'date_collected_utc'])
                 channel_name = file.strip('.csv')
                 df['channel_name'] = channel_name
                 result_df = pd.concat([result_df, df])
     return result_df
-    
     
                 
 def main()-> None:
@@ -109,8 +171,8 @@ def main()-> None:
     #         df.to_csv(join('cleaned_datasets', file), index=False )
     # print(get_video_duration('PT1H30M17S'))
     ## Cleaning Youtuber_channel_details.csv
-    df = data_cleaning_channel_details('datasets/youtuber_channel_details.csv')
-    df.to_csv(join('cleaned_datasets', 'youtuber_channel_details.csv' ), index=False )
+    # df = data_cleaning_channel_details('datasets/youtuber_channel_details.csv')
+    # df.to_csv(join('cleaned_datasets', 'youtuber_channel_details.csv' ), index=False )
     ## Compiling all the dataframes
     # df = data_compilation('cleaned_datasets', 
     #                  ['video_title', 'date_released_utc', 'view_count', 'like_count',
@@ -118,6 +180,8 @@ def main()-> None:
     #                   'video_duration_M', 'video_duration_S', 'channel_name'],
     #                   ['youtuber_channel_details.csv'])
     # df.to_csv('compiled_datasets/compiled_df.csv', index=False)
+    ## Test
+    print(get_num('1.2k'))
 
 
 if __name__ == '__main__':
